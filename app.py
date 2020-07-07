deb15922c695d2aaa4dba2469e8b4c22b449df9f
VISHNUPRIYA <vishnupriya0598@gmail.com>
2020-07-07 16:54:53 +0530

    Fixed for JWT importing error

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from db import db
 from flask import Flask
 from flask_restful import Api
-from flask import JWT
+from flask_jwt import JWT
 
 from security import authenticate,identity
 from resources.user import UserRegister
@@ -12,6 +12,8 @@ from resources.store import Store,StoreList
 
 
 app=Flask(__name__)
+
+#app.config['DEBUG']=True
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db' #SQLAlchemy creates a db and place in data.db so no need to use create_tables.py
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #Avoid changes
 app.secret_key='xyz'
